9bf7ac8b0f7940ad88c9ac77b4a3807ea35b5f37
Victoria Lopez <vlopez@alumnos.exa.unicen.edu.ar>
2023-09-26 18:59:53 -0300

    agrego carrito de compras

--- tp2/js/sidebar.js
+++ tp2/js/sidebar.js
@@ -1,5 +1,18 @@
 let menuSide = document.getElementById('menu-side');
 let btnOpen = document.getElementById('btn-open');
+let btnUser = document.getElementById('btn-user');
+let menuUser = document.getElementById('menu-user');
+let btnBuy = document.getElementById('btn-buy');
+let cart = document.getElementById('cart');
+
+btnBuy.addEventListener('click', function() {
+    cart.classList.toggle('open');
+});
+
+btnUser.addEventListener('click', function() {
+    //menuSide.classList.toggle('menu-side-hidden');
+    menuUser.classList.toggle('open');
+});
 
 btnOpen.addEventListener('click', function() {
     menuSide.classList.toggle('menu-side-hidden');
@@ -13,6 +26,7 @@ if(window.innerWidth < 400) {
 
 window.addEventListener('resize', function() {
     if(window.innerWidth < 400) {
+        
         menuSide.classList.toggle('menu-side-hidden');
     } else {
         menuSide.classList.remove('menu-side-hidden');
