0410ee7b3da6d6696055bbdb90998b01f544e687
Euge <meuparodi@gmail.com>
2023-10-04 15:49:31 -0300

    agrergo animacion botones, loading, y registro

--- tp2/js/loading.js
+++ tp2/js/loading.js
@@ -1,32 +1,47 @@
 // Obtén referencia al botón y a la imagen
-var button1 = document.getElementById('log-in');
-var button2 = document.getElementById('log-google');
-var button3 = document.getElementById('log-fb');
+let button1 = document.getElementById('log-in');
+let button2 = document.getElementById('log-google');
+let button3 = document.getElementById('log-fb');
+let loading = document.getElementById('loading');
 
 // Agrega un evento click al botón
 button1.addEventListener('click', function() {
-    // Muestra la imagen
-    var popUp = window.open('./loading.html');
-    // Espera 3 segundos antes de redirigir a otra página
+    loading.classList.remove('no-visible');        
+    loading.classList.add('visible');
     setTimeout(function() {
-        // Redirige a la nueva página (reemplaza 'nueva_pagina.html' con la URL deseada)
-        popUp.close();
-        window.location.href = './index.html';
-    }, 2000); // 2000 milisegundos (2 segundos)
+            window.location.href = './index.html';
+    }, 5000); 
 });
 
 button2.addEventListener('click', function() {
-    var popUp = window.open('./loading.html');
+    loading.classList.remove('no-visible');        
+    loading.classList.add('visible');
     setTimeout(function() {
-        popUp.close();
-        window.location.href = './index.html';
-    }, 2000); 
+            window.location.href = './index.html';
+    }, 5000); 
 });
 
 button3.addEventListener('click', function() {
-    var popUp = window.open('./loading.html');
+    loading.classList.remove('no-visible');        
+    loading.classList.add('visible');
     setTimeout(function() {
-        popUp.close();
-        window.location.href = './index.html';
-    }, 2000); 
+            window.location.href = './index.html';
+    }, 5000); 
 });
+
+
+// function actualizarPorcentaje() {
+//     let porcentaje = 10; 
+//     let porcentajeElement = document.getElementById('porcentaje');
+
+//     let intervalo = setInterval(function() {
+//         if (porcentaje < 100) {
+//             porcentaje += 10; 
+//             porcentajeElement.textContent = 'cargando...' + porcentaje + '%';
+//         } else {
+//             clearInterval(intervalo); 
+//         }
+//     }, 5000); 
+// }
+
+// actualizarPorcentaje();
