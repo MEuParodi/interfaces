43f16f4deb3b3d696f3efdecf52dff0734af01c7
Victoria Lopez <vlopez@alumnos.exa.unicen.edu.ar>
2023-10-07 03:04:27 -0300

    termine carrousel cards oferta

--- tp2/js/relGames.js
+++ tp2/js/relGames.js
@@ -1,6 +1,11 @@
 let related = document.getElementById('related-games');
 let prev = document.querySelectorAll('img.prev');
 let next = document.querySelectorAll('img.next');
+let shareIcon = document.getElementById('shareIcon');
+let controls = document.getElementById('play-controls');
+let shareSection = document.querySelector('.share');
+let arrowClose = document.querySelectorAll('.arrow-close');
+let instructionControls = document.querySelector('.instruction-controls');
 
 function scrollCarousel(cant){
     //para scrollear el carrusel
@@ -24,4 +29,32 @@ if(window.innerWidth < 400){
 }
 
 
-related.addEventListener('click',scrollLeft(150));
+function closePopUp(section){
+    section.classList.toggle('open');
+}
+
+arrowClose.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(elem.offsetParent == shareSection){
+            closePopUp(shareSection);
+        }
+        if(elem.offsetParent == instructionControls){
+            closePopUp(instructionControls);
+        }
+        
+    })
+});
+
+shareIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+    closePopUp(shareSection)
+});
+
+controls.addEventListener('click', (e) => {
+    e.preventDefault();
+    closePopUp(instructionControls)
+});
+
+
+related.addEventListener('click', () => {scrollLeft(150)});
