d78380312e23776f81700a65fe8e0eb36d123edd
Euge <meuparodi@gmail.com>
2023-11-05 18:06:17 -0300

    agrego popup

---
+++ tp3/js/4enLinea.js/popup.js
@@ -0,0 +1,14 @@
+// Accede al elemento del mensaje emergente
+let popup = document.getElementById('game-msg');
+let mostrar = document.getElementById('popup-btn');
+let msg = document.getElementById('msgPopup');
+
+// Cambia el texto del mensaje emergente
+msg.innerHTML = "Turno del Jugador 1";
+
+mostrar.addEventListener('click', () =>{
+    popup.style.display = "block";
+      setTimeout(function() {
+          popup.style.display = "none";
+    }, 1000);
+});
