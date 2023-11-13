cea7f29fae859681863ca0f7b4a60d1161bfc3b4
Euge <meuparodi@gmail.com>
2023-11-13 15:08:43 -0300

    agrego loading page

---
+++ tp4/js/loader.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", loadingCircle);
+
+function actualizarPorcentaje() {
+    let porcentaje = 10; 
+    let porcentajeElement = document.getElementById('porcentaje');
+
+    let intervalo = setInterval(function() {
+        if (porcentaje < 100) {
+            porcentaje += 10; 
+            porcentajeElement.textContent = 'cargando...' + porcentaje + '%';         } else {
+            clearInterval(intervalo); 
+        }
+        }, 500); 
+}
+
+
+function loadingCircle (e) {
+        e.preventDefault();
+        loading.classList.remove('no-visible');        
+        loading.classList.add('visible');
+        actualizarPorcentaje();
+        setTimeout(function() {
+            window.location.href = './index.html';
+        }, 5000);
+}
+
+
+
+
+
+
