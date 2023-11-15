fd6ec61590c529accb5e7406ba4e79b750a8adec
Euge <meuparodi@gmail.com>
2023-11-15 14:35:48 -0300

    animacion seccion7

--- tp4/js/section7_pop-up.js
+++ tp4/js/section7_pop-up.js
@@ -10,15 +10,21 @@ let closePopup1 = document.getElementById("close1");
 let closePopup2 = document.getElementById("close2");
 let closePopup3 = document.getElementById("close3");
 
+let section7 = document.getElementById('section7');
+
+
 spiderWhite.addEventListener('click', function(){
-   popupStacy.classList.add('show');
+    section7.style.backgroundImage = 'none';
+    popupStacy.classList.add('show');
 });
 
 spiderRed.addEventListener('click', function(){
+    section7.style.backgroundImage = 'none';
     popupPeter.classList.add('show');
 });
 
 spiderBlack.addEventListener('click', function(){
+    section7.style.backgroundImage = 'none';
     popupMorales.classList.add('show');
 });
  
@@ -34,4 +40,43 @@ closePopup2.addEventListener('click', function(){
 closePopup3.addEventListener('click', function(){
     popupMorales.classList.remove('show');
         
+});
+
+
+spiderWhite.addEventListener('mouseover', function(){
+    section7.style.backgroundImage = 'url("./img/seccion7/pink_bground.png")';
+    section7.style.backgroundSize = 'cover';
+    section7.style.backgroundRepeat = 'no-repeat';
+    spiderWhite.style.transform = 'scale(1.1)';
+    spiderWhite.style.filter= 'blur(0)';
+    spiderRed.style.transform = 'scale(0.7)';
+    spiderRed.style.filter= 'blur(5px)';
+    spiderBlack.style.transform = 'scale(0.7)';
+    spiderBlack.style.filter= 'blur(5px)';
+});
+
+spiderRed.addEventListener('mouseover', function(){
+    section7.style.backgroundImage = 'url("./img/seccion7/blue_bground.png")';
+    section7.style.backgroundSize = 'cover';
+    section7.style.backgroundRepeat = 'no-repeat';
+    spiderRed.style.transform= 'scale(1.1)';
+    spiderRed.style.filter= 'blur(0)';
+    spiderWhite.style.transform = 'scale(0.7)';
+    spiderWhite.style.filter= 'blur(5px)';
+    spiderBlack.style.transform = 'scale(0.7)';
+    spiderBlack.style.filter= 'blur(5px)';
+
+});
+
+spiderBlack.addEventListener('mouseover', function(){
+    section7.style.backgroundImage = 'url("./img/seccion7/grey_bground.png")';
+    section7.style.backgroundSize = 'cover';
+    section7.style.backgroundRepeat = 'no-repeat';
+    spiderBlack.style.transform ='scale(1.1)';
+    spiderBlack.style.filter= 'blur(0)';
+    spiderWhite.style.transform = 'scale(0.7)';
+    spiderWhite.style.filter= 'blur(5px)';
+    spiderRed.style.transform = 'scale(0.7)';
+    spiderRed.style.filter= 'blur(5px)';
+
 });
